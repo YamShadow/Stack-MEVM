d854e96856f7c20bfe4e97175a8513c70e6d9ec2
Mathieu Nibas <mnibas.ext@wedigital.garden>
2019-06-12 22:49:45 +0200

    Feat(Time): Implement timer

--- pwa/src/views/Home.vue
+++ pwa/src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <div>
+      <a class="u-link-white mb-20" href="#" @click="toggleTimer">{{ time }}</a>
+      <div>
+        <a class="rounded-full py-2 px-4 bouton openSans font-bold" href="#" @click="reset">reset</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,6 +15,49 @@
 
 export default {
   name: "home",
-  components: {}
+  components: {},
+  data() {
+    return {
+      time: 10,
+      isRunning: false,
+      interval: null
+    };
+  },
+  methods: {
+    toggleTimer() {
+      if (this.isRunning) {
+        clearInterval(this.interval);
+        console.log("timer stops");
+      } else {
+        this.interval = setInterval(this.incrementTime, 1000);
+      }
+      this.isRunning = !this.isRunning;
+    },
+    incrementTime() {
+      this.time = parseInt(this.time) - 1;
+      if (this.time == 0) {
+        clearInterval(this.interval);
+        this.isRunning = !this.isRunning;
+        console.log("SAVVVVVVEE");
+      }
+    },
+    reset() {
+      this.time = 10;
+    }
+  }
 };
 </script>
+
+
+<style scoped lang="scss">
+.bouton {
+  width: 50%;
+  margin: 20px auto;
+  padding: 15px;
+}
+.contrast {
+  background-color: #4500b6;
+  color: white;
+}
+</style>
+
